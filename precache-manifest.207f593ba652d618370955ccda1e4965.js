dd03cf99c3532ee0d615c4876028133a6ead1404
Olena Savchenko <olsavchenko1@gmail.com>
2020-06-21 15:47:09 +0300

    Updates

--- precache-manifest.207f593ba652d618370955ccda1e4965.js
+++ precache-manifest.207f593ba652d618370955ccda1e4965.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b094f0e82df0c0fba42aa6a956e9437a",
+    "revision": "56a0e09c84546c38b064d42f0bba4547",
     "url": "/goit-react-hw-01-components/index.html"
   },
   {
-    "revision": "e78a07dcff0128becd15",
+    "revision": "7f5a1b51fadadf4d15ac",
     "url": "/goit-react-hw-01-components/static/css/2.4b502e0b.chunk.css"
   },
   {
-    "revision": "077e9f51b5d689786e7e",
-    "url": "/goit-react-hw-01-components/static/css/main.e95946ec.chunk.css"
+    "revision": "6e135425e16f575f77a7",
+    "url": "/goit-react-hw-01-components/static/css/main.fdd7985a.chunk.css"
   },
   {
-    "revision": "e78a07dcff0128becd15",
-    "url": "/goit-react-hw-01-components/static/js/2.6b429dd2.chunk.js"
+    "revision": "7f5a1b51fadadf4d15ac",
+    "url": "/goit-react-hw-01-components/static/js/2.e4b14e9a.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/goit-react-hw-01-components/static/js/2.6b429dd2.chunk.js.LICENSE.txt"
+    "url": "/goit-react-hw-01-components/static/js/2.e4b14e9a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "077e9f51b5d689786e7e",
-    "url": "/goit-react-hw-01-components/static/js/main.b1a90739.chunk.js"
+    "revision": "6e135425e16f575f77a7",
+    "url": "/goit-react-hw-01-components/static/js/main.e01c66ff.chunk.js"
   },
   {
     "revision": "cf184cad0926235fce4a",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-01-components/precache-manifest.fb7a999298afd3281c28a1a0c94768ba.js"
+  "/goit-react-hw-01-components/precache-manifest.207f593ba652d618370955ccda1e4965.js"
 );
 
 self.addEventListener('message', (event) => {
